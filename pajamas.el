67797efdbd97b93b6ea1e10bc350c6c42d8fd8fc
Trevor Murphy <trevor.m.murphy@gmail.com>
2025-03-17 21:16:47 -0700

    Check in a rough sketch of the library.
    This is mainly boilerplate comments and some implementations of basic
    functions.  There's a lot missing still, but good to have something.

---
+++ pajamas.el
@@ -0,0 +1,106 @@
+;;; pajamas.el --- Wrap your dev commands in a warm blanket  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2025
+;; Version: 0.0.1
+;; Package-Requires: ((project dash))
+
+;; Author:  <trevor.m.murphy@gmail.com>
+;; Keywords: convenience, tools, lisp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This file contains generic infrastructure for dealing with build and
+;; test commands on a project-by-project basis.
+;;
+;; The goal is to have one key that just "does the right thing" based on
+;; `project-current'.
+;;
+;; Commands:
+;;
+;; `pajamas-build' will default to Emacs' `compile' but can be
+;; customized to any other command
+;;
+;; `pajamas-test' defaults to "make test" but can be customized to any
+;; other command
+
+;;; Code:
+
+(require 'cl-generic)
+
+(defgroup pajamas nil
+  "Build commands on the current project."
+  :version "30.1"
+  :group 'tools)
+
+;;; Customization:
+
+(defvar pajamas-find-functions (list #'pajamas--try-fallback)
+  "Special hook to find the build system in the current project.
+
+Each function on this hook is called in turn with one argument, the directory
+in which to look, and should return either nil to mean that it is no
+applicable, or a pajamas instance.  The exact form of the pajamas instance is
+up to each respective function; the only practical limitation is to use values
+that `cl-defmethod' can dispatch on, like a cons cell, or a list, or a CL
+struct.")
+
+(defvar pajamas-common-backend-markers-alist
+  '(("Makefile" . Make)
+    ("GNUMakefile" . Make)
+    ("Eldev" . Eldev))
+  "Assoc list assigning marker files to common backend symbols.")
+
+;;; Internal Variables:
+
+
+
+;;; Commands:
+
+
+;;; Internal Functions:
+
+(defun pajamas--try-fallback (dir)
+  "Try searching above DIR for files indicating a common build system."
+  (let* ((backend-markers (->> pajamas-common-backend-markers-alist
+                               (mapcar 'car-safe)
+                               (delete nil)))
+         (marker-re (rx-to-string
+                     `(seq string-start (or ,@backend-markers)
+                           string-end)))
+         match
+         (root
+          (locate-dominating-file
+           dir
+           (lambda (d)
+             (setq match
+                   (condition-case nil
+                       (car (directory-files d nil marker-re t 1))
+                     (file-missing nil))))))
+         (backend (cdr (assoc match pajamas-common-backend-markers-alist))))
+    (when backend (cons backend root))))
+
+;;; Public Functions:
+
+(cl-defgeneric pajamas-build (project)
+  "Call an appropriate build command for PROJECT."
+  (compile))
+
+(cl-defgeneric pajamas-test (project)
+  "Call an appropriate test command for PROJECT.")
+
+(provide 'pajamas)
+;;; pajamas.el ends here
+
